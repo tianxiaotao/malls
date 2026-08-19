74ddb8512735086b6aecd0483fd2678a4282709c
田小涛 <tianloveguoxi@hotmail.com>
2026-08-19 16:02:48 +0800

    Update 商品详情.py

--- web/Vvic/商品详情.py
+++ web/Vvic/商品详情.py
@@ -13,7 +13,7 @@ headers = {
     'Accept-Language':'en,zh-CN;q=0.9,zh;q=0.8,ja;q=0.7',
     'Referer':'https://www.vvic.com/',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-    'Cookie': 'source=m;userLoginAuto=1;vvic_token=5129759f-1d9b-4815-8c34-88487e267db6;uid=2862702;userName=vvic9153980633;umc=1;pn=0'
+    'Cookie': 'source=m;userLoginAuto=1;vvic_token=xxxxxxxx-1d9b-4815-8c34-xxxxxxx;uid=00000;userName=vvic915300000;umc=1;pn=0'
 }
 
 
